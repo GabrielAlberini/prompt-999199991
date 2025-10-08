2d9a0d1330695235039f717b6e6e5592dd87c919
gabrielalberini <gabialberini733@gmail.com>
2025-10-08 17:06:24 -0300

    actualización manejo de ruta

--- clase-5/frontend/src/components/PublicRoute.jsx
+++ clase-5/frontend/src/components/PublicRoute.jsx
@@ -1,15 +1,15 @@
 import { Navigate } from "react-router-dom"
 import { useAuth } from "../context/AuthContext"
 
-export const PrivateRoute = ({ children }) => {
+export const PublicRoute = ({ children }) => {
   const { user, loading } = useAuth()
 
   if (loading) {
     return <div>Cargando...</div>
   }
 
-  if (!user) {
-    return <Navigate to="/login" />
+  if (user) {
+    return <Navigate to="/" />
   }
 
   return children
